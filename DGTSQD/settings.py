2914f89ceb722ed07a4bdb2e2675357e5b6533d8
KAVIN KUMAR R <60545605+KAVINDIMO@users.noreply.github.com>
2020-12-11 18:35:20 +0530

    deleted db

--- DGTSQD/settings.py
+++ DGTSQD/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'DGTSQD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
-#POSTGRES
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'digit',
-        'USER': 'postgres',
-        'PASSWORD': 'kavin',
-        'listen_addresses':'*',
-        'HOST': '127.0.0.1,52.173.149.254',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#POSTGRES
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'digit',
+        #'USER': 'postgres',
+        #'PASSWORD': 'kavin',
+        #'listen_addresses':'*',
+        #'HOST': '127.0.0.1,52.173.149.254',
+        #'PORT': '5432',
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
